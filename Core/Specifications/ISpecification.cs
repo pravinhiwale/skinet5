9479964d2810b98366fbd5e8d924202e1b978184
Pravin Hiwale <Pravin.Hiwale@yardi.com>
2022-01-02 18:25:39 +1100

    End of section 6.1

---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+         Expression<Func<T,bool>> Criteria {get;set;} //this is our where clause
+         List<Expression<Func<T,object>>> Includes {get;set;} //this will be in include 
+         Expression<Func<T,object>> OrderBy {get;}
+         Expression<Func<T,object>> OrderByDescending {get;set;}
+         int Take{get;}
+         int Skip {get;}
+         bool IsPagingEnabled{get;}
+         
+
+    }
+}
